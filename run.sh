5b6b1c63e664b6d52fafdfb5d49d42719358b262
Forever-Tango <ARJUNAGITABG@GMAIL.COM>
2025-04-15 21:19:27 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,13 @@
 
+#!/bin/bash
+
+echo "Compiling..."
+make
+
+echo "Creating output directory..."
+mkdir -p output
+
+echo "Running edge detection and restoration..."
+./signal_processing > log.txt
+
+echo "Done. Output saved in output/ and log.txt"
